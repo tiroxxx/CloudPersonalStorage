01d1f41bd9b41b36cb9d62c90b1ff2ef67393aa2
Christian Marquez <christianmarquez41@gmail.com>
2021-03-06 16:30:37 -0500

    fontawesome npm package installed

--- src/components/cloud-drive/AddFolderBtn.js
+++ src/components/cloud-drive/AddFolderBtn.js
@@ -1,15 +1,17 @@
 import { Button } from 'react-bootstrap'
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFolderPlus } from "@fortawesome/free-solid-svg-icons"
 
 export default function AddFolderBtn() {
 
     function openModal() {
-        
+
     }
 
     return (
         <Button onClick={openModal} variant="outline-success" size="sm">
-            asdasd
+            <FontAwesomeIcon icon={faFolderPlus} />
         </Button>
     )
 }
