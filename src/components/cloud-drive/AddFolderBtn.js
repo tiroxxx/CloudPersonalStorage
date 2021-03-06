62082ee55ce37550219d872266d32edd8e434c39
Christian Marquez <christianmarquez41@gmail.com>
2021-03-06 16:33:55 -0500

    added functions to open and close the modal

--- src/components/cloud-drive/AddFolderBtn.js
+++ src/components/cloud-drive/AddFolderBtn.js
@@ -1,18 +1,29 @@
-import { Button } from 'react-bootstrap'
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Modal } from 'react-bootstrap'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faFolderPlus } from "@fortawesome/free-solid-svg-icons"
 
 export default function AddFolderBtn() {
 
-    function openModal() {
+    const [open, setOpen] = useState(false)
 
+    function openModal() {
+        setOpen(true)
+    }
+
+    function closeModal() {
+        setOpen(flase)
     }
 
     return (
-        <Button onClick={openModal} variant="outline-success" size="sm">
-            <FontAwesomeIcon icon={faFolderPlus} />
-        </Button>
+        <>
+            <Button onClick={openModal} variant="outline-success" size="sm">
+                <FontAwesomeIcon icon={faFolderPlus} />
+            </Button>
+            <Modal>
+
+            </Modal>
+        </>
     )
 }
 
