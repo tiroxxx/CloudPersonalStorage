7800760adc26c847d9dda72f8a263b67a629b88f
Christian Marquez <christianmarquez41@gmail.com>
2021-03-07 15:58:17 -0500

    added action to set child folders in folder hook

--- src/hooks/useFolder.js
+++ src/hooks/useFolder.js
@@ -1,9 +1,11 @@
 import { useEffect, useReducer } from "react";
+import { useAuth } from "../contexts/AuthContext";
 import { database } from "../firebase";
 
 const ACTIONS = {
     SELECT_FOLDER: "select-folder",
-    UPDATE_FOLDER: "update-folder"
+    UPDATE_FOLDER: "update-folder",
+    SET_CHILD_FOLDERS: "set-child-folders"
 }
 
 const ROOT_FOLDER = { name: "Root", id: null, path: [] }
@@ -22,6 +24,11 @@ function reducer(state, { type, payload }) {
                 ...state,
                 folder: payload.folder
             }
+        case ACTIONS.SET_CHILD_FOLDERS:
+            return {
+                ...state,
+                childFolders: payload.childFolders
+            }
         default:
             return state
     }
@@ -34,6 +41,7 @@ export function useFolder(folderId = null, folder = null) {
         childFolders: [],
         childFiles: []
     })
+    const { currentUser } = useAuth()
 
     useEffect(() => {
         dispatch({ type: ACTIONS.SELECT_FOLDER, payload: { folderId, folder } })
@@ -62,6 +70,19 @@ export function useFolder(folderId = null, folder = null) {
                 })
             })
     }, [folderId])
+
+    useEffect(() => {
+        const cleanup = database.folders
+            .where("parentId", "==", folderId)
+            .where("userId", "==", currentUser.uid)
+            .orderBy("createdAt")
+            .onSnapshot(snapshot => {
+                dispatch({
+                    type: ACTIONS.SET_CHILD_FOLDERS,
+                    payload: { childFolders: snapshot.docs.map(database.formatDoc) }
+                })
+            })
+    }, [folderId, currentUser])
 
     return state
 }
