730e7e91047d12226ef9fa517b15cc11093f0e72
Christian Marquez <christianmarquez41@gmail.com>
2021-03-09 18:29:45 -0500

    files now get replaced if the name and location are the same

--- src/components/cloud-drive/AddFileBtn.js
+++ src/components/cloud-drive/AddFileBtn.js
@@ -59,13 +59,26 @@ export default function AddFileBtn({ currentFolder }) {
             })
 
             uploadTask.snapshot.ref.getDownloadURL().then(url => {
-                database.files.add({
-                    url: url,
-                    name: file.name,
-                    createdAt: database.getCurrentTimestamp(),
-                    folderId: currentFolder.id,
-                    userId: currentUser.uid
-                })
+                database.files
+                    .where("name", "==", file.name)
+                    .where("userId", "==", currentUser.uid)
+                    .where("folderId", "==", currentFolder.id)
+                    .get()
+                    .then(existingFiles => {
+                        const existingFile = existingFiles.docs[0]
+                        if (existingFile) {
+                            existingFile.ref.update({ url: url })
+                        }
+                        else {
+                            database.files.add({
+                                url: url,
+                                name: file.name,
+                                createdAt: database.getCurrentTimestamp(),
+                                folderId: currentFolder.id,
+                                userId: currentUser.uid
+                            })
+                        }
+                    })
             })
         })
     }
@@ -87,7 +100,14 @@ export default function AddFileBtn({ currentFolder }) {
                         }}
                     >
                         {uploadingFiles.map(file => (
-                            <Toast key={file.id}>
+                            <Toast key={file.id} onClose={() => {
+                                setUploadingFiles(prevUploadingFiles => {
+                                    return prevUploadingFiles.filter(uploadFile => {
+                                        return uploadFile.id !== file.id
+                                    })
+                                })
+                            }}
+                            >
                                 <Toast.Header
                                     closeButton={file.error}
                                     className="text-truncate w-100 d-block"
